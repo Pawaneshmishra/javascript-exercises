64a827ae7bffc49915c0ecfe5bb4dd5e9b76c436
Pawaneshmishra <mpawanesh@gmail.com>
2024-04-26 23:11:27 +0530

    1-7 JS exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for(const item of items){
+        for(let index=array.length-1; index>=0; index--){
+            if(array[index] === item){
+                array.splice(index,1);
+            }
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
